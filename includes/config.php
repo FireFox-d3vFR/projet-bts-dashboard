bf12298a66f1fa041cafb7f98fc2e2beec659a3c
Jonathan_mqhm <137342185+FireFox-d3vFR@users.noreply.github.com>
2024-06-14 10:40:23 +0200

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+// Informations de connexion à la base de données
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'instrumentation-systemes');
