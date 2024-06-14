f783aebce9472bf3dcd6e19dbd2a441c664d8ba2
Jonathan_mqhm <137342185+FireFox-d3vFR@users.noreply.github.com>
2024-06-14 10:45:58 +0200

    Create footer.php

---
+++ templates/footer.php
@@ -0,0 +1,34 @@
+<footer class="bg-light text-center py-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5 class="mb-3">Liens Utiles</h5>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="index.php" class="footer-link">Accueil</a></li>
+                    <li><a href="contact.php" class="footer-link">Contact</a></li>
+                    <li><a href="about.php" class="footer-link">À propos</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5 class="mb-3">Suivez-nous</h5>
+                <a href="#" class="text-dark mx-2 footer-link"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="text-dark mx-2 footer-link"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="text-dark mx-2 footer-link"><i class="fab fa-linkedin-in"></i></a>
+                <a href="#" class="text-dark mx-2 footer-link"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="col-md-4">
+                <h5 class="mb-3">Contactez-nous</h5>
+                <p class="mb-1">Adresse : 1234 Rue Exemple, Ville, Pays</p>
+                <p class="mb-1">Téléphone : +33 1 23 45 67 89</p>
+                <p class="mb-1">Email : contact@instrumentation-systemes.fr</p>
+            </div>
+        </div>
+        <hr class="my-4">
+        <p>&copy; 2024 Instrumentation Systèmes. Tous droits réservés.</p>
+    </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6HJTYoR0tY6L6jI2JT5zwI4FttIMxqsXY7fWUh+e0IGmoCpH1K" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIybnf7Q2xzB/l9FIOV90oWibOWVnZymfucbwp9" crossorigin="anonymous"></script>
+</body>
+</html>
